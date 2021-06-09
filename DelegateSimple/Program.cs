50655b682f96c46a1ee597e1162af52898a25a2f
Alain-Duplan <aduplan@umass.edu>
2021-06-08 20:25:58 -0400

    sql

---
+++ DelegateSimple/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DelegateSimple
+{
+    public class MethodClass{
+        public void method1(){
+            Console.WriteLine("This is method 1");
+        }
+         public void method2(){
+            Console.WriteLine("This is method 2");
+        }
+         public void method3(){
+            Console.WriteLine("This is method 3");
+        }
+         public int method4(string message){
+            Console.WriteLine("This is method 4");
+            return 1;
+        }
+        public int method5 (string message){
+            Console.WriteLine("this is method 5");
+            string m = Console.ReadLine();
+            return 2;
+        }
+
+         public int method6 (string message){
+            Console.WriteLine("this is method 5");
+            message += Console.ReadLine();
+            return 3;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateSimple myDelegateClass = new DelegateSimple();
+            MethodClass myMethodClass = new MethodClass();
+
+            /*myDelegateClass.mySimpleDelegate = myMethodClass.method1;
+            myDelegateClass.mySimpleDelegate += myMethodClass.method2;
+            myDelegateClass.mySimpleDelegate += myMethodClass.method3;
+            myDelegateClass.mySimpleDelegate -= myMethodClass.method2;
+            */
+
+            //myDelegateClass.mySimpleDelegate();
+            //Console.WriteLine(myDelegateClass.mySimpleDelegate.GetInvocationList().ToString());
+            
+            myDelegateClass.myNotSimpleDelegate = myMethodClass.method4;
+            myDelegateClass.myNotSimpleDelegate += myMethodClass.method5;
+            myDelegateClass.myNotSimpleDelegate += myMethodClass.method6;
+            int result = myDelegateClass.myNotSimpleDelegate("generic");
+    }
+}
+}
---
+++ P0/P0/Item.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+
+namespace P0{
+    public class Item{
+        
+        public string name {get; set;}
+        public double price{get; set;}
+        public string description{get; set;}
+        
+        public Item(string name, double price, string description = "unavailable"){
+            this.name = name;
+            this.price = price;
+            this.description = description;
+        }
+    }
+    }
+
